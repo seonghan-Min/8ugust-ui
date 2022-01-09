c6bc071d6903c9aeb9b74e00cd543e20d5ca0a90
shmin <gks831@naver.com>
2022-01-10 00:07:42 +0900

    [#0003] Main페이지 Side-Bar 개발 01

---
+++ include/header.php
@@ -0,0 +1,3 @@
+<meta charset="utf-8"></meta>
+<link href="asset/css/common.css" rel="stylesheet" type="text/css">
+<link href="asset/css/sb-admin-2.css" rel="stylesheet" type="text/css">
--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php ?>
+<html lang="ko">
+    <head>
+        <?php include("include/header.php"); ?>
+    </head>
+    <body>
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+            <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+                
+            </ul>
+        </div>
+        <!-- Page Wrapper -->
+    </body>
+</html>
