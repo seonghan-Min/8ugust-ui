f8ef9df232a92d92bfd4b1c90e1cad3cf8dd0a18
shmin <gks831@naver.com>
2022-01-13 23:59:05 +0900

    [#0005] Nav바 로고 추가

--- index.php
+++ index.php
@@ -7,7 +7,12 @@
         <!-- Page Wrapper -->
         <div id="wrapper">
             <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-                
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+                    <div class="sidebar-brand-icon rotate-n-15">
+                        <i class="fas fa-laugh-wink"></i>
+                    </div>
+                    <div class="sidebar-brand-text mx-3">8ugust</div>
+                </a>
             </ul>
         </div>
         <!-- Page Wrapper -->
