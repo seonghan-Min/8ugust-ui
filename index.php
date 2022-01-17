0e3c45a83abcabda9ebe63d081b32b66adc6f11d
shmin <gks831@naver.com>
2022-01-18 00:12:14 +0900

    [#0006] 사이드바 메인 추가

--- index.php
+++ index.php
@@ -8,11 +8,16 @@
         <div id="wrapper">
             <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
                 <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-                    <div class="sidebar-brand-icon rotate-n-15">
-                        <i class="fas fa-laugh-wink"></i>
-                    </div>
                     <div class="sidebar-brand-text mx-3">8ugust</div>
                 </a>
+                <hr class="sidebar-divider my-0">   <!-- divider -->
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">
+                        <i class="icon-home"></i>
+                        <span>Home</span>
+                    </a>
+                </li>
+                <hr class="sidebar-divider my-0">   <!-- divider -->
             </ul>
         </div>
         <!-- Page Wrapper -->
