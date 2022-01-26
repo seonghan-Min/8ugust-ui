626e4ee3dab1959b45c6b61a11d87534db987618
shmin <gks831@naver.com>
2022-01-27 00:50:47 +0900

    [#0008] TOP 메뉴 개발

--- index.php
+++ index.php
@@ -2,12 +2,75 @@
 <html lang="ko">
     <head>
         <? include($_SERVER["DOCUMENT_ROOT"]."/include/header.php"); ?>
+        <script type="text/javascript" src="/asset/js/index.js"></script>
     </head>
     <body>
         <!-- Page Wrapper -->
         <div id="wrapper">
+            <!-- Side Bar -->
             <? include($_SERVER["DOCUMENT_ROOT"]."/include/sidebar.php"); ?>
+            <!-- Side Bar -->
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
+                <!-- Content -->
+                <div id="content">
+                    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                        <!-- Top Search Bar -->
+                        <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                            <div class="input-group">
+                                <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="button">
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        <!-- Top Search Bar -->
+                        <!-- Top User Bar -->
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item dropdown no-arrow">
+                                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                    <img class="img-profile rounded-circle" src="">
+                                </a>
+                                <!-- Dropdown - User Information -->
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown" id="userDropdown_div">
+                                    <a class="dropdown-item" href="#">
+                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Profile
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Settings
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Activity Log
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Logout
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                        <!-- Top User Bar -->
+                    </nav>
+                </div>
+                <!-- Content -->
+                <!-- Footer -->
+                <footer class="sticky-footer bg-white">
+
+                </footer>
+                <!-- Footer -->
+            </div>
+            <!-- Content Wrapper -->
         </div>
         <!-- Page Wrapper -->
     </body>
+    <script>
+        index_init();
+    </script>
 </html>
