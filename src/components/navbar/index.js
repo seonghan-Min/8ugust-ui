626356b9ed49fbbc9db09405c96a22cbed9e88d4
shmin <gks831@naver.com>
2022-02-12 23:58:40 +0900

    [#0001] Nav바 HTML 완성

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import styles from "./index.module.css";
+import styles from './index.module.css';
+import profile from '../../img/profile.png';
 
 function Navbar () {
 
     return (
-        <div>
-            <div>
+        <div className={styles['navbar']}>
+            <div className={styles['nav-header']}>
+                <img className={styles['nav-header-img']} src={profile} alt='profile_img'/>
                 <h3>8ugust</h3>
             </div>
-            <nav className={styles['navbar']}>
-                <ul className={styles['nav-menu']}>
-                    <li className={styles['nav-item']}>HI</li>
-                    <li className={styles['nav-item']}>HI</li>
-                    <li className={styles['nav-item']}>HI</li>
-                    <li className={styles['nav-item']}>HI</li>
-                    <li className={styles['nav-item']}>HI</li>
-                </ul>
-            </nav>
+            <div className={styles['nav-menu']}>
+                <div className={styles['nav-item']}># HI</div>
+                <div className={styles['nav-item']}># HI</div>
+                <div className={styles['nav-item']}># HI</div>
+                <div className={styles['nav-item']}># HI</div>
+            </div>
+            <div className={styles['nav-footer']}>
+                <div className={styles['nav-footer-wrap']}>
+                    <div className={styles['nav-footer-txt']}>Create Room</div>
+                    <button className={styles['nav-footer-btn']}>+</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Navbar from './components/navbar/index';
-import page_image from './img/page_image.png';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
-    <App />
-    <img src={page_image} alt='my page'/>
+    <div className='content'>
+      <Navbar />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
