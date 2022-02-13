a1e3bc1d175f7b089133a70d1eef8f3cb25406ed
shmin <gks831@naver.com>
2022-02-13 11:10:17 +0900

    [#0002] content footer 작성

--- src/components/body-content/index.js
+++ src/components/body-content/index.js
@@ -5,13 +5,16 @@ function BodyContent () {
     return (
         <div className={styles['body-content']}>
             <div className={styles['content-header']}>
-                aaa
+                <div className={styles['content-header-txt']}># HI</div>
             </div>
             <div className={styles['content-main']}>
                 bbb
             </div>
             <div className={styles['content-footer']}>
-                ccc
+                <div className={styles['content-footer-txt']}>
+                    <input placeholder='Type a Message..' className={styles['footer-input']}></input>
+                </div>
+                <div className={styles['content-footer-btn']}>ddd</div>
             </div>
         </div>
     )
