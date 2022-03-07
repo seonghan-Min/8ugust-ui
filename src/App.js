ccece7a55ed3cf44cfd97ebc2eb7634ae3fe1163
shmin <gks831@naver.com>
2022-03-08 01:46:13 +0900

    [BLOG-002] Header 작업 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link, Route } from 'react-router-dom';
 import instagram from './img/Instagram_logo.png';
+import github from './img/Github_logo.png';
+import profile from './img/profile.png';
 import './index.css';
 
 function App() {
@@ -36,10 +38,24 @@ function App() {
 					</div>
 				</div>
 				<div className='site'>
-					<img src={instagram}></img>
+					<img src={instagram} onClick={() => window.location.href="https://www.instagram.com/8ugust_run/"}></img>
+					<img src={github} onClick={() => window.location.href="https://github.com/seonghan-Min"}></img>
 				</div>
 			</header>
-			<div className='content-wrap'>fdsafdsfdasfds</div>
+			<div className='content-wrap'>
+				<div className='left'>
+					dsfasddfsa
+				</div>
+				<div className='right'>
+					<div className='instagram'>
+						<div>
+							<img src={profile}></img> 
+							<p>8ugust_run</p>
+						</div>
+						<img src={profile}></img>
+					</div>
+				</div>
+			</div>
 		</>
   );
 }
