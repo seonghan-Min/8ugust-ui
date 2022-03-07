458f3976fa47e1ff2ce468299eeb861d4a1af620
shmin <gks831@naver.com>
2022-03-07 23:48:20 +0900

    [BLOG-001] Header 작업 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link, Route } from 'react-router-dom';
+import instagram from './img/Instagram_logo.png';
+import './index.css';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
+	return (
+		<>
+			<header>
+				<div className='site-title'>
+					<h1>8UGUST.LOG</h1>
+					<div>DEVELOPMENT & LIFE</div>
+				</div>
+				<div className='menu-container-pc'>
+					<div>
+						<p>Introduce</p>
+					</div>
+					<div>
+						<p>Knowledge</p>
+						<span>▼</span>
+					</div>
+					<div>
+						<p>Development</p>
+						<span>▼</span>
+					</div>
+					<div>
+						<p>Life</p>
+						<span>▼</span>
+					</div>
+				</div>
+				<div className='menu-container-mobile'>
+					<div>
+						<p>Main Menu</p>
+						<span>▼</span>
+					</div>
+				</div>
+				<div className='site'>
+					<img src={instagram}></img>
+				</div>
+			</header>
+			<div className='content-wrap'>fdsafdsfdasfds</div>
+		</>
   );
 }
 
